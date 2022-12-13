99feab04ccbc19825ce3400eb99a7e6b303e0e16
nadirmemmedov20 <nadirmemmedov35@gmail.com>
2022-12-13 10:30:55 -0800

    add Day7Task

---
+++ Day7Task/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Task.Model
+{
+    public class User
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string WriteEmail(string firstName, string lastName)
+        {
+            return $"{firstName}{lastName}@gmail.com".ToLower();
+        }
+    }
+
+}
---
+++ Day7Task/Program.cs
@@ -0,0 +1,30 @@
+﻿using Day7Task.Model;
+
+public class Program
+{
+    public static void Main()
+    {
+        User user = new User
+        {
+            FirstName = "Nadir",
+            LastName = "Memmedov"
+        };
+        user.GetType().GetProperties().ToList().ForEach(p=>
+        {
+            if(p.Name!="Email")
+            {
+                Console.WriteLine($"{p.Name}: {p.GetValue(user)}");
+            }
+        }
+           
+        );
+        user.GetType().GetMethods().ToList().ForEach(op =>
+        {
+            if(op.Name == "WriteEmail")
+            {
+                Console.Write("Email: ");
+                Console.WriteLine(op.Invoke(user, new object[] { user.FirstName, user.LastName }));
+            }
+        });
+    }
+}
