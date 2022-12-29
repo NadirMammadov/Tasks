39ca80a2225927afa8d74b6531854bda791e7ad8
nadir <nadir@MACHINE>
2022-12-29 21:49:01 +0400

    Update SqlTaskDay13

--- Sql/SqlTaskDay13.sql
+++ Sql/SqlTaskDay13.sql
@@ -14,9 +14,10 @@ select FirstName from Employees where Right(FirstName,1) !='T'
 select FirstName from Employees Where FirstName Not LIKE  '[A-I]%'
 
 -- Adının 2. harfi A veya T olmayanlar
-select FirstName from Employees Where FirstName Not LIKE  '[A-I]%'
+select FirstName from Employees Where FirstName Not LIKE  '_A%' and FirstName Not LIKE  '_T%'
 
 --Adının ilk iki harfi LA, LN, AA veya AN olanlar
 select FirstName from Employees Where FirstName Like 'LA%' or FirstName Like 'LN%' or FirstName Like 'AA%' or FirstName Like 'AN%' 
+
 -- Adının içerisinde _ geçen isimleri listeleyiniz 
 SELECT FirstName FROM EMPLOYEES WHERE FirstName LIKE '%\_%' ESCAPE '\';
