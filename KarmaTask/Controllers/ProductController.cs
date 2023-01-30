587dbd22df4b48c3e07dd82150a26b6bf14793c2
nadir <nadir@MACHINE>
2023-01-30 21:54:23 +0400

    add KarmaTask

---
+++ KarmaTask/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KarmaTask.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
